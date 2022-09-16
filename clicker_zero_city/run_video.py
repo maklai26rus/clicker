934f04b8027cb2de8f0145a71601e281e7c4e86a
Maklai <maklai26rus@mai.ru>
2022-09-17 00:16:44 +0300

    скрипт работает. Доделываю мелкие нюанс и начинаю переходить к полевым тестам

--- clicker_zero_city/run_video.py
+++ clicker_zero_city/run_video.py
@@ -6,15 +6,16 @@ import os
 from command_clicker import *
 from search_coordinate import search_coordinate
 
-# isClicking = False
 video_rooms = 'png/reklama.PNG'
 video_tablet = 'png/reklama2.PNG'
 prize = 'png/prize.PNG'
+
 rooms = "png/study_rooms"
 dining = "png/study_rooms/dining_room"
 lab = "png/study_rooms/lab_room"
 bank = "png/study_rooms/bank_room"
 zal = "png/study_rooms/zal_room"
+joinery = "png/study_rooms/joinery"
 
 path_rooms = [rooms + "/" + file for file in os.listdir(rooms)]
 
@@ -22,8 +23,9 @@ path_dining_room = [dining + "/" + file for file in os.listdir(dining)]
 path_lab_room = [lab + "/" + file for file in os.listdir(lab)]
 path_bank_room = [bank + "/" + file for file in os.listdir(bank)]
 path_zal_room = [zal + "/" + file for file in os.listdir(zal)]
+path_joinery_room = [joinery + "/" + file for file in os.listdir(joinery)]
 
-path = [path_dining_room, path_lab_room, path_bank_room, path_zal_room]
+path = [path_joinery_room, path_dining_room, path_lab_room, path_bank_room, path_zal_room, ]
 
 keyboard.add_hotkey('ALT + Z', advertising_rooms)
 keyboard.add_hotkey('ALT + X', advertising_tablet)
@@ -59,23 +61,35 @@ def choose_rooms():
 
 
 def main():
+    one_time_click = True
     while True:
         if choosing_action["rooms"]:
             room = choose_rooms()
             if room:
-                pyautogui.click(room)
+                if one_time_click:
+                    pyautogui.click(room)
+                time.sleep(5)
                 click_rooms = definition_advertising_rooms()
                 if click_rooms:
                     pyautogui.click(click_rooms)
                     time.sleep(35)
+                    one_time_click = False
                 else:
                     del path[0]
+                    one_time_click = True
 
         if choosing_action['tablet']:
-            click_tablet = definition_prize()
+            click_tablet = definition_advertising_tablet()
+
             if click_tablet:
+                time.sleep(1)
                 pyautogui.click(click_tablet)
                 time.sleep(35)
+
+            click_prize = definition_prize()
+            if click_prize:
+                time.sleep(1)
+                pyautogui.click(click_prize)
 
 
 if __name__ == '__main__':
--- clicker_zero_city/var_orangeria.py
+++ clicker_zero_city/var_orangeria.py
@@ -1,11 +1,21 @@
 import pyautogui
 import time
 
+try:
+    import easyocr
+except UserWarning:
+    print("ошибка модуля")
+
 global_map = "png/global_maps.PNG"
 kater = "png/kater.PNG"
+stop = "png/stop.PNG"
 orang = "png/orang.PNG"
 battle = 'png/battle.PNG'
 battle_next = 'png/battle_next.PNG'
+update = 'png/update.PNG'
+my_strength = 3400000
+
+reader = easyocr.Reader(['ru'])
 
 
 def time_game(func):
@@ -29,7 +39,8 @@ def global_maps():
     :return:
     """
     click = pyautogui.locateOnScreen(global_map, confidence=0.5)
-    pyautogui.click(click)
+    if click:
+        pyautogui.click(click)
     time.sleep(1)
 
     if greenhouse_search():
@@ -38,18 +49,27 @@ def global_maps():
         boat_search()
         greenhouse_search()
 
-    run_battle()
+    update_map()
+    get_enemy()
 
 
 def boat_search():
     """
-    Ищем на карте катер и привязываемся кнему что бы перетащить мышкку
+    Ищем на карте катер и привязываемся к нему что бы перетащить мышкку
     :return:
     """
     click = pyautogui.locateOnScreen(kater, confidence=0.4)
     if click:
         pyautogui.click(click)
         pyautogui.dragTo(100, 0, 1, button='left')
+
+
+def update_map():
+    # click = pyautogui.locateOnScreen(update, confidence=0.7)
+    click = pyautogui.locateOnScreen(update, confidence=0.9)
+    # pyautogui.click(click)
+    pyautogui.click(click)
+    time.sleep(1)
 
 
 def greenhouse_search():
@@ -63,26 +83,56 @@ def greenhouse_search():
         time.sleep(10)
 
 
-def run_battle():
+def get_enemy():
     """
-    Начинаем бой в оранжереи
-    :return:
+    Определение пративника по его силе.
+    в my_strength указана максимальная сила для нападения
     """
-    click = list(pyautogui.locateAllOnScreen(battle, confidence=0.93))
-    for b in click:
-        center = pyautogui.center(b)
-        pyautogui.click(center)
-        time.sleep(0.2)
-        start_battle_orangeria()
-        time.sleep(5)
+    number_temp = 1
+    path_temp = f'png/temp/{number_temp}.png'
+    battle_click = list(pyautogui.locateAllOnScreen(battle, confidence=0.9, grayscale=True))
+
+    for enemy in battle_click:
+        pyautogui.screenshot(path_temp, region=(enemy.left - 390, enemy.top, enemy.width + 130, enemy.height + 10))
+        time.sleep(0.1)
+        enemy_strength = reader.readtext(path_temp, detail=0)
+        es_int = int(enemy_strength[1].replace(' ', ''))
+        if my_strength >= es_int:
+            center = pyautogui.center(enemy)
+            pyautogui.click(center)
+            time.sleep(0.2)
+
+            start_battle_orangeria()
+
+            time.sleep(5)
+
+        number_temp += 1
 
 
 def start_battle_orangeria():
+    """Определяет если активна кнопка или выйти с выбора """
     click = pyautogui.locateOnScreen(battle_next, confidence=0.6)
     if click:
         pyautogui.click(click)
+        time.sleep(4)
+
+        stop_battle()
+
         time.sleep(20)
         pyautogui.press('esc')
+
     else:
         pyautogui.press('esc')
+
+
+def stop_battle():
+    """
+    Если энергия закончилась
+    кликер закрывает окно
+    """
+    battle_stop = pyautogui.locateOnScreen(stop, confidence=0.6)
+    if battle_stop:
+        time.sleep(0.1)
+        pyautogui.press('esc')
+
 
