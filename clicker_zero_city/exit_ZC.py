9d138cc1aed5f22d857eeedfb843a3fc80f15507
Maklai <maklai26rus@mai.ru>
2022-09-24 10:13:22 +0300

    Добавил возможность закрывать прогрумму в начале перед запуском. Если произойдет сбой то программа при повторном запуске не зависнет

--- clicker_zero_city/exit_ZC.py
+++ clicker_zero_city/exit_ZC.py
@@ -6,3 +6,10 @@ def exit_zc():
 
     for process in (process for process in psutil.process_iter() if process.name() == "ZC.exe"):
         process.kill()
+
+
+def exit_mgl():
+    """Закрытия процеса ZC.exe """
+
+    for process in (process for process in psutil.process_iter() if process.name() == "MGL.exe"):
+        process.kill()
