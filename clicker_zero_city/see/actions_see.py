32371b05739593afbb56f7b6229f7fbed948f44e
Maklai <maklai26rus@mail.ru>
2022-10-03 14:49:35 +0300

    Разобрался с кнопками старт и стоп. теперь программа работает в поточном режиме

--- clicker_zero_city/see/actions_see.py
+++ clicker_zero_city/see/actions_see.py
@@ -1,7 +1,7 @@
 import time
 
 from run_video import AdClicker
-from see import Ui_MainWindow
+from see_ad import Ui_MainWindow
 from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtCore import QThread
 import sys
@@ -11,23 +11,22 @@ class ProgresBar(QThread):
 
     def __init__(self, mainwindow):
         super().__init__()
+        QThread.__init__(self)
+        self.action = True
+        self.program_operation_switch = False
         self.mainwindow = mainwindow
 
-    def watch_ads(self, action, program_operation_switch):
-        self.mainwindow._set_enabled(action)
+    def run(self):
+        self.watch_ads()
 
-        self.mainwindow.preview.KITCHEN_ROOM = self.mainwindow.kitchen_room.isChecked()
-
-        self.mainwindow.preview.LAB_ROOM = self.mainwindow.lab_room.isChecked()
-
-        self.mainwindow.preview.BANK_ROOM = self.mainwindow.bank_room.isChecked()
-
-        self.mainwindow.preview.ZAL_ROOM = self.mainwindow.zal_room.isChecked()
-
-        self.mainwindow.preview.JOINERY_ROOM = self.mainwindow.sawmill_room.isChecked()
-
-        while program_operation_switch:
-            print(self.mainwindow.preview.KITCHEN_ROOM)
+    def watch_ads(self):
+        while self.program_operation_switch:
+            print(self.mainwindow.preview.KITCHEN_ROOM, 'Kitchen')
+            print(self.mainwindow.preview.LAB_ROOM, 'LAB_ROOM')
+            print(self.mainwindow.preview.JOINERY_ROOM, 'JOINERY_ROOM')
+            print(self.mainwindow.preview.ZAL_ROOM, 'ZAL_ROOM')
+            print(self.mainwindow.preview.BANK_ROOM, 'BANK_ROOM')
+            print(self.mainwindow.preview.FORGE_ROOM, 'FORGE_ROOM')
             time.sleep(3)
             # if self.preview.KITCHEN_ROOM:
             #     self.preview.kitchen_rooms()
@@ -42,10 +41,20 @@ class ProgresBar(QThread):
             # elif self.preview.FORGE_ROOM:
             #     self.preview.forge_room()
 
+    def method_name(self):
+        self.mainwindow._set_enabled(self.action)
+        self.mainwindow.preview.KITCHEN_ROOM = self.mainwindow.kitchen_room.isChecked()
+        self.mainwindow.preview.LAB_ROOM = self.mainwindow.lab_room.isChecked()
+        self.mainwindow.preview.BANK_ROOM = self.mainwindow.bank_room.isChecked()
+        self.mainwindow.preview.ZAL_ROOM = self.mainwindow.zal_room.isChecked()
+        self.mainwindow.preview.JOINERY_ROOM = self.mainwindow.sawmill_room.isChecked()
+        self.mainwindow.preview.FORGE_ROOM = self.mainwindow.forge_room.isChecked()
+
 
 class ActionsSee(Ui_MainWindow):
 
     def __init__(self):
+        super().__init__()
         self.preview = AdClicker()
         self.program_operation_switch = False
 
@@ -56,37 +65,21 @@ class ActionsSee(Ui_MainWindow):
         self.start_rooms.clicked.connect(self.when_viewing_ads)
 
     def when_viewing_ads_stop(self):
-        # self.program_operation_switch = False
-        self.bar.watch_ads(action=True, program_operation_switch=False)
-        # action = True
-        # self._set_enabled(action)
+        self.bar.action = True
+        self.bar.program_operation_switch = False
+        self.bar.method_name()
+
     def when_viewing_ads(self):
         """
         Начала просмотре рекламы
         """
-        self.bar.watch_ads(action=False, program_operation_switch=True)
-        # action = False
-        #
-        # self._set_enabled(action)
-        #
-        # self.preview.KITCHEN_ROOM = self.kitchen_room.isChecked()
-        #
-        # self.preview.LAB_ROOM = self.lab_room.isChecked()
-        #
-        # self.preview.BANK_ROOM = self.bank_room.isChecked()
-        #
-        # self.preview.ZAL_ROOM = self.zal_room.isChecked()
-        #
-        # self.preview.JOINERY_ROOM = self.sawmill_room.isChecked()
-        # self.watch_ads()
-        # self.bar = ProgresBar(mainwindow=self)
+        self.bar.action = False
+        self.bar.program_operation_switch = True
+        self.bar.start()
+        self.bar.method_name()
 
     def stop_ads_room(self):
-        # self.program_operation_switch = False
         self.stop_rooms.clicked.connect(self.when_viewing_ads_stop)
-
-
-
 
     def _set_enabled(self, action):
         self.kitchen_room.setEnabled(action)
@@ -95,24 +88,6 @@ class ActionsSee(Ui_MainWindow):
         self.zal_room.setEnabled(action)
         self.sawmill_room.setEnabled(action)
         self.forge_room.setEnabled(action)
-        # print(self.forge_room.text())
-
-    # def watch_ads(self):
-    #     while self.xxx:
-    #         if self.preview.KITCHEN_ROOM:
-    #             self.preview.kitchen_rooms()
-    #         elif self.preview.LAB_ROOM:
-    #             self.preview.laboratory_room()
-    #         elif self.preview.JOINERY_ROOM:
-    #             self.preview.joinery_room()
-    #         elif self.preview.ZAL_ROOM:
-    #             self.preview.zal_room()
-    #         elif self.preview.BANK_ROOM:
-    #             self.preview.bank_room()
-    #         elif self.preview.FORGE_ROOM:
-    #             self.preview.forge_room()
-    #         else:
-    #             self.watch_ads()
 
 
 def main():
@@ -121,20 +96,9 @@ def main():
     ui = ActionsSee()
     ui.setupUi(MainWindow)
 
-    ui.xx = ProgresBar(ui)
-
-    ui.watch_ads_room()
-    ui.stop_ads_room()
-    # ui.watch_ads()
-
     MainWindow.show()
     sys.exit(app.exec_())
-
-    # if choosing_action['tablet']:
-    #     preview.preview_tablet()
-    #     preview.definition_prize()
 
 
 if __name__ == "__main__":
     main()
-    # watch_ads()
