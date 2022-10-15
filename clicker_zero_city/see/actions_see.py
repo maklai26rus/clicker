22d822085db2b89a4f00a2b417bf78486fb328c8
Maklai <maklai26rus@mai.ru>
2022-10-15 14:06:26 +0300

    устранена ошибка с мигающей кнопкой

--- clicker_zero_city/see/actions_see.py
+++ clicker_zero_city/see/actions_see.py
@@ -43,31 +43,31 @@ class Inspector(QThread):
         while self.program_operation_switch:
             time.sleep(0.5)
             if self.choosing_action["rooms"]:
-                self.mainwindow.btn_changes_color_startup(self.mainwindow.preview.KITCHEN_ROOM,
-                                                          self.mainwindow.btn_kitchen_test)
-                self.mainwindow.btn_changes_color_startup(self.mainwindow.preview.LAB_ROOM,
-                                                          self.mainwindow.btn_lab_test)
-                self.mainwindow.btn_changes_color_startup(self.mainwindow.preview.SAWMILL_ROOM,
-                                                          self.mainwindow.btn_sawmill_test)
-                self.mainwindow.btn_changes_color_startup(self.mainwindow.preview.ZAL_ROOM,
-                                                          self.mainwindow.btn_zal_test)
-                self.mainwindow.btn_changes_color_startup(self.mainwindow.preview.BANK_ROOM,
-                                                          self.mainwindow.btn_bank_test)
-                self.mainwindow.btn_changes_color_startup(self.mainwindow.preview.FORGE_ROOM,
-                                                          self.mainwindow.btn_forge_test)
 
                 if self.mainwindow.preview.KITCHEN_ROOM:
                     self.mainwindow.preview.kitchen_rooms()
+                    self.mainwindow.btn_changes_color_startup(self.mainwindow.preview.KITCHEN_ROOM,
+                                                              self.mainwindow.btn_kitchen_test)
                 elif self.mainwindow.preview.LAB_ROOM:
                     self.mainwindow.preview.laboratory_room()
+                    self.mainwindow.btn_changes_color_startup(self.mainwindow.preview.LAB_ROOM,
+                                                              self.mainwindow.btn_lab_test)
                 elif self.mainwindow.preview.SAWMILL_ROOM:
                     self.mainwindow.preview.joinery_room()
+                    self.mainwindow.btn_changes_color_startup(self.mainwindow.preview.SAWMILL_ROOM,
+                                                              self.mainwindow.btn_sawmill_test)
                 elif self.mainwindow.preview.ZAL_ROOM:
                     self.mainwindow.preview.zal_room()
+                    self.mainwindow.btn_changes_color_startup(self.mainwindow.preview.ZAL_ROOM,
+                                                              self.mainwindow.btn_zal_test)
                 elif self.mainwindow.preview.BANK_ROOM:
                     self.mainwindow.preview.bank_room()
+                    self.mainwindow.btn_changes_color_startup(self.mainwindow.preview.BANK_ROOM,
+                                                              self.mainwindow.btn_bank_test)
                 elif self.mainwindow.preview.FORGE_ROOM:
                     self.mainwindow.preview.forge_room()
+                    self.mainwindow.btn_changes_color_startup(self.mainwindow.preview.FORGE_ROOM,
+                                                              self.mainwindow.btn_forge_test)
 
             elif self.choosing_action['tablet']:
                 self.mainwindow.preview.cheking_tablet()
